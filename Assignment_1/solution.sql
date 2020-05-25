f01350609ef005021028e2e1021317f327165a18
fmuno003 <munozf88@gmail.com>
2020-05-25 13:27:39 -0400

    Updated Name for grader and created copy statements to copy test data into tables and test constraints

--- Assignment_1/solution.sql
+++ Assignment_1/solution.sql
@@ -9,9 +9,14 @@
 ** --   --------   -------   ------------------------------------
 ** 1    05/25/2020 fmuno003  Initial Creation
 *********************************************************************/
+-- COMPLETE TEST CASE 1: Insert Normal Data.
+-- TEST CASE 2: Insert non-exist foreign key.
+-- TEST CASE 3: Insert duplicate rating.
+-- TEST CASE 4: Insert a hasagenre record that contains wrong genre id.
+-- TEST CASE 5: Insert a rating larger than 5.
 
 -- Checks to see if tables exist already or not. If they do, drop tables
-DROP TABLE IF EXISTS HasAGenre;
+DROP TABLE IF EXISTS hasagenre;
 DROP TABLE IF EXISTS tags;
 DROP TABLE IF EXISTS ratings;
 DROP TABLE IF EXISTS genres;
@@ -22,25 +27,25 @@ DROP TABLE IF EXISTS users;
 -- Table for Users
 CREATE TABLE users (
     userid INT PRIMARY KEY,
-    name VARCHAR(50)
+    name text NOT NULL
 );
 
 -- Table for Movies
 CREATE TABLE movies (
     movieid INT PRIMARY KEY,
-    title VARCHAR(50)
+    title text
 );
 
 -- Table for Tag Info
 CREATE TABLE taginfo(
     tagid INT PRIMARY KEY,
-    content VARCHAR(200)
+    content text
 );
 
 -- Table for Genres
 CREATE TABLE genres(
     genreid INT PRIMARY KEY,
-    name VARCHAR(200) 
+    name text 
 );
 
 -- Table for Ratings
@@ -57,7 +62,7 @@ CREATE TABLE ratings(
 CREATE TABLE tags(
     userid INT,
     movieid INT,
-    tagid INT,
+    tagid INT PRIMARY KEY,
     FOREIGN KEY(userid) REFERENCES users(userid),
     FOREIGN KEY(movieid) REFERENCES movies(movieid),
     FOREIGN KEY(tagid) REFERENCES taginfo(tagid),
@@ -65,9 +70,34 @@ CREATE TABLE tags(
 );
 
 -- Table for Having a Genre
-CREATE TABLE HasAGenre(
+CREATE TABLE hasagenre(
     movieid INT,
     genreid INT,
     FOREIGN KEY(movieid) REFERENCES movies(movieid),
     FOREIGN KEY(genreid) REFERENCES genres(genreid)
 );
+
+
+
+
+-- This will only be used for testing purposes only
+\copy users from 'C:/Users/Francisco/Desktop/CSE511 - DataProcessingAtScale/CSE511/Assignment_1/TestData/users.dat' DELIMITER '%' CSV;
+SELECT COUNT(*) FROM users;
+
+\copy movies from 'C:/Users/Francisco/Desktop/CSE511 - DataProcessingAtScale/CSE511/Assignment_1/TestData/movies.dat' DELIMITER '%' CSV;
+SELECT COUNT(*) FROM movies;
+
+\copy taginfo from 'C:/Users/Francisco/Desktop/CSE511 - DataProcessingAtScale/CSE511/Assignment_1/TestData/taginfo.dat' DELIMITER '%' CSV;
+SELECT COUNT(*) FROM taginfo;
+
+\copy genres from 'C:/Users/Francisco/Desktop/CSE511 - DataProcessingAtScale/CSE511/Assignment_1/TestData/genres.dat' DELIMITER '%' CSV;
+SELECT COUNT(*) FROM genres;
+
+\copy ratings from 'C:/Users/Francisco/Desktop/CSE511 - DataProcessingAtScale/CSE511/Assignment_1/TestData/ratings.dat' DELIMITER '%' CSV;
+SELECT COUNT(*) FROM ratings;
+
+\copy tags from 'C:/Users/Francisco/Desktop/CSE511 - DataProcessingAtScale/CSE511/Assignment_1/TestData/tags.dat' DELIMITER '%' CSV;
+SELECT COUNT(*) FROM tags;
+
+\copy hasagenre from 'C:/Users/Francisco/Desktop/CSE511 - DataProcessingAtScale/CSE511/Assignment_1/TestData/hasagenre.dat' DELIMITER '%' CSV;
+SELECT COUNT(*) FROM hasagenre;
